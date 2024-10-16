9c1e403fd6431f661d5b4aff6cadcba6b0b28de0
Alumno Facultad de Ingenieria L1 <alum.fial1@lim.upeu.edu.pe>
2024-10-15 19:04:43 -0500

    mensaje

---
+++ db.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'123456',
+    database:'bdnode'
+});
+connection.connect((err)=>{
+    if (err) {
+        console.log("Error de conexion a MYSQL: ",err);
+        return;
+    }else(
+        console.log("Conectado a BD MYSQL")
+    )
+});
